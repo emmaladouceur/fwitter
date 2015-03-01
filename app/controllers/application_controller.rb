1844c8a9215cd4e72f18d21d2f6eddf168055d6f
emmaladouceur <eladouceur@brearley.org>
2015-03-01 13:48:37 -0500

    error and login pages added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 require './config/environment'
 require './app/models/tweet'
+require './app/models/user'
+require 'pry'
 
 class ApplicationController <
 	Sinatra::Base
@@ -7,24 +9,58 @@ class ApplicationController <
 configure do 
 	set :public_folder, 'public'
 	set :views, 'app/views'
+	enable :sessions
+	set :session_secret, "fwitter"
 	end
 
-	get '/' do 
+	get '/login' do
+		erb :login
+	end 
+
+	post "/login" do
+		@user = User.find_by(:username => params[:username], :email => params[:email])
+		if @user
+			session[:user_id] = @user.id
+			redirect('/users')
+		else 
+			erb :error
+		end
+	end
+
+	get '/lougout' do
+		session[:user_id] = nil
+		redirect('/login')
+	end
+
+
+	get '/tweets' do 
 		"Welcome to Fwitter"
 		
-		@tweets = Tweet.all_messages
+		@tweets = Tweet.all
+		@users = User.all
 
 
 		erb :tweets
 
 	end
 
-	post '/tweet' do
+	post '/tweets' do
 		# puts params
 		# binding.pry
-		new_tweet = Tweet.new(params[:user], params[:message])
-		redirect ('/')
+		new_tweet = Tweet.new(:user_id => params[:user_id], :message => params[:message])
+		new_tweet.save
+		redirect ('/tweets')
 	end	
 
+	get '/users' do
+		@logged_in_user = User.find(session[:user_id])
+		@users = User.all
+		erb :users
+	end 
 
+	post '/users' do
+		new_user = User.new(:username => params[:username], :email => params[:email], :profile_picture => params[:profile_picture], :age => params[:age])
+		new_user.save
+		redirect('/users')
+	end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,40 +1,5 @@
-class Tweet
-
-	attr_accessor :user, :message
-
-	ALL_MESSAGES = []
-
-	def initialize(user, message)
-		@user = user
-		@message = message
-		ALL_MESSAGES << self
-	end
-
-	def self.all_messages
-		ALL_MESSAGES
-	end
-
-
-	# def user
-	# 	@user
-	# end
-
-	# def user=(user)
-	# 	@user = user
-	# end
-
-	# def message
-	# 	@message
-	# end
-
-	# def message=(message)
-	# 	@message = message
-	# end
-
+class Tweet < ActiveRecord::Base
+	belongs_to :user
 end
-
-
-
-
 
 
